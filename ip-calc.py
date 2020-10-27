df219035d11e49c561ddac514730223bbcddf31a
Mouhab-dev <mohab.taher@yahoo.com>
2020-10-27 16:01:30 +0200

    Update ip-calc.py

--- ip-calc.py
+++ ip-calc.py
@@ -2,49 +2,92 @@
     This script is programmed by Mouhab-dev
     Follow me on Github: https://github.com/mouhab-dev
 """
+# For colored output
+CRED = '\033[91m'
+CEND = '\033[0m'
+
 # Function to convert Decimal to Binary
 def dtb(n):
-    return bin(n).replace("0b", "")
+    return format(n,'08b')
 
 print("Welcome to IP Calculator v1.0 by Mouhab-dev")
 print("Project Repo on Github: https://github.com/mouhab-dev/ip-calculator")
 print("Find me on Github: https://github.com/mouhab-dev\n")
 
-# Ask user for ip address and then split it based on the '.' and put it in a list
-try:
-    ip = list(map(int,input("Please, Enter an IP Address: ").split('.')))
-except ValueError:
-    print("Error: Only integers are allowed")
-    exit(1)
+ip=[] # List to store ip address
+check_ip=True # Variabke to manage loop over ip address
+while check_ip:
+    try:
+        # Ask user for ip address and then split it based on the '.' and put it in a list
+        ip = list(map(int,input("Please, Enter an IP Address: ").split('.')))
+        check_ip=False
+        # error handling for ip address
+        if len(ip) != 4 :
+            # Check for X.X.X.X Format
+            print(CRED + "Error: IP Address supported format X.X.X.X" + CEND)
+            check_ip=True
+            continue
+        for i in ip:
+            # Check if the any digit exceeds 255
+            if i > 255:
+                print(CRED + "Error: IP Address Range: (0-255)" + CEND)
+                check_ip=True
+                break
+        continue
+    except ValueError:
+        print(CRED + "Error: Only integers are allowed" + CEND)
+        check_ip=True
+        continue
+    except KeyboardInterrupt:
+        print()
+        print(CRED + "IP Calculator has been terminated." + CEND)
+        exit(0)
+    except:
+        print(CRED + "An error has occurred." + CEND)
+        check_ip=True
+        continue
 
-# error handling for ip address
-for i in ip:
-    if i > 255:
-        raise ValueError("Sorry, This is not a valid IP Address")
-
-if len(ip) != 4 :
-    raise Exception("IP Address is not valid")
-
-# Ask user for subnet mask and then split it based on the '.' and put it in a list
-try:
-    subnet = list(map(int,input("Please, Enter Subnet Mask: ").split('.')))
-except ValueError:
-    print("Error: Only integers are allowed")
-    exit(11)
-
-# error handling for subnet
-for j in subnet:
-    if j > 255:
-        raise ValueError("Sorry, This is not a valid Subnet Mask")
-
-if len(subnet) != 4 :
-    raise Exception("Subnet Mask is not valid")
+subnet=[] # List to store subnet mask
+check_subnet=True # Variable to manage loop over subnet mask
+while(check_subnet):
+    try:
+        # Ask user for subnet mask and then split it based on the '.' and put it in a list
+        subnet = list(map(int,input("Please, Enter Subnet Mask: ").split('.')))
+        check_subnet=False
+        # error handling for subnet
+        if len(subnet) != 4:
+            print(CRED + "Error: Subnet Mask supported format X.X.X.X" + CEND)
+            check_subnet=True
+            continue
+        for j in subnet:
+            # Check if the digits exceeds 255
+            if j > 255:
+                print(CRED + "Error: Subnet Mask Range: (0-255)" + CEND)
+                check_subnet=True
+                break
+            # Check for invalid subnetmask (continous binary ones)
+            if '01' in dtb(j):
+                print(CRED + "Error: Invalid subnet mask, it must contain continous ones." + CEND)
+                check_subnet=True
+                break
+        continue
+    except ValueError:
+        print(CRED + "Error: Only integers are allowed." + CEND)
+        check_subnet=True
+        continue
+    except KeyboardInterrupt:
+        print()
+        print(CRED + "IP Calculator has been terminated." + CEND)
+        exit(0)
+    except:
+        print(CRED + "An error has occurred." + CEND)
+        check_subnet=True
+        continue
 
 # full_bin_subnet=""
 
 subnet_ones=0 # count no of ones in subnet mask
 t_no_hosts=0 # calculate total number of hosts
-
 network_id=[] # list to store network id
 broadcast_address=[0,0,0,0] # list to store broadcast address
 
@@ -69,9 +112,6 @@ for i in range(4):
         broadcast_address[i] = network_id[i]
     else:
         broadcast_address[i] = 255 - subnet[i] + network_id[i]
-
-
-# print(full_bin_subnet)
 
 print("--------------------------------------------")
 
